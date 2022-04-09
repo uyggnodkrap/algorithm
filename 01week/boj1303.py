5323b1d52d66e5afe6ec5feef2d66522712ffccd
uyggnodkrap <uyggnodkrap26@gmail.com>
2022-04-09 16:03:03 +0900

    boj

--- 01week/boj1303.py
+++ 01week/boj1303.py
@@ -7,13 +7,14 @@ from collections import deque
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-def dfs_w(graph, x, y):
 
+def bfs(x, y, team):
         queue = deque(())
         queue.append((x, y))
 
-        graph[x][y] = 0
+        graph[x][y] = "0"
         cnt = 1
+
         while queue:
             x, y = queue.popleft()
 
@@ -21,31 +22,27 @@ def dfs_w(graph, x, y):
                 nx = x + dx[i]
                 ny = y + dy[i]
 
-                if nx < 0 or nx >= s or ny < 0 or ny >= t:
+                if nx < 0 or ny < 0 or nx >= n or ny >= m:
                     continue
-                if graph[nx][ny] == 1:
-                    graph[nx][ny] = 0
+                if graph[nx][ny] != 0 and graph[nx][ny] == team:
                     queue.append((nx, ny))
+                    graph[nx][ny] = 0
                     cnt += 1
 
-        return cnt
+        return cnt**2
 
-s, t = map(int, input().split(' '))
-graph = []
+m, n = map(int, input().split(' '))
+graph = [list(input().strip()) for _ in range(n)]
 
-for _ in range(s):
-    graph.append(list(input()))
+w, b = 0, 0
 
-for g in graph:
-    print(g)
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] != "0":
+            if graph[i][j] == "W":
+                w += bfs(i, j, "W")
 
-# 1. W
-w, b = [], []
-for i in range(s):
-    for j in range(t):
-        if graph[i][j] == "W":
-            w.append(dfs_w(graph, i, j))
-print(w)
-# 2. B
+            elif graph[i][j] == "B":
+                b += bfs(i, j, "B")
 
-
+print(w, b)
