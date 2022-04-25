8d1711e9e89729e5fd99362cee43dac116906358
uyggnodkrap <uyggnodkrap26@gmail.com>
2022-04-26 00:05:58 +0900

    완전탐색/이분탐색

--- algorithmstudy/02week/prgm42839.py
+++ algorithmstudy/02week/prgm42839.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
 소수 찾기
 https://programmers.co.kr/learn/courses/30/lessons/42839
@@ -29,3 +30,36 @@ def solution(numbers):
     prime = isPrime(max(answer))
 
     return len([a for a in answer if a in prime])
+=======
+"""
+소수 찾기
+https://programmers.co.kr/learn/courses/30/lessons/42839
+"""
+
+from itertools import permutations
+
+
+def isPrime(x):
+    prime = [False, False] + [True] * (x - 1)
+
+    for i in range(2, x + 1):
+        if prime[i]:
+            for j in range(i + i, x + 1, i):
+                prime[j] = False
+
+    return [i for i in range(len(prime)) if prime[i]]
+
+
+def solution(numbers):
+    numbers = list(numbers)
+    answer = set()
+
+    for i in range(1, len(numbers) + 1):
+        arr = set(permutations(numbers, i))
+        for a in arr:
+            answer.add(int("".join(a)))
+
+    prime = isPrime(max(answer))
+
+    return len([a for a in answer if a in prime])
+>>>>>>> a81508b (완전탐색/이분탐색)
