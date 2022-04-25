10b34d0221372f4df5a9ea0226e945603c37f865
uyggnodkrap <uyggnodkrap26@gmail.com>
2022-04-26 00:05:39 +0900

    완전탐색/이분탐색

--- algorithmstudy/01week/prgm42583.py
+++ algorithmstudy/01week/prgm42583.py
@@ -2,3 +2,31 @@
 다리를 지나는 트럭
 https://programmers.co.kr/learn/courses/30/lessons/42583
 """
+from collections import deque
+
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    bridge = deque([0] * bridge_length)
+    truck_weights = deque(truck_weights)
+    bridge_weight = 0
+
+    while truck_weights:
+        answer += 1
+        next_truck = bridge.popleft()
+        bridge_weight -= next_truck
+
+        if bridge_weight + truck_weights[0] > weight:
+            bridge.append(0)
+
+        else:
+            truck = truck_weights.popleft()
+            bridge.append(truck)
+            bridge_weight += truck
+
+    while bridge_weight > 0:
+        answer += 1
+        next_truck = bridge.popleft()
+        bridge_weight -= next_truck
+
+    return answer
--- algorithmstudy/02week/boj10816.py
+++ algorithmstudy/02week/boj10816.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """
 숫자 카드 2
 https://www.acmicpc.net/problem/10816
@@ -15,4 +16,23 @@ for c in card:
     if c in hash_card.keys():
         hash_card[c] += 1
 
+=======
+"""
+숫자 카드 2
+https://www.acmicpc.net/problem/10816
+"""
+
+import sys
+_ = int(sys.stdin.readline())
+card = list(sys.stdin.readline().rstrip().split(' '))
+_ = int(sys.stdin.readline())
+arr = list(sys.stdin.readline().rstrip().split(' '))
+
+hash_card = {a: 0 for a in arr}
+
+for c in card:
+    if c in hash_card.keys():
+        hash_card[c] += 1
+
+>>>>>>> c307b07 (완전탐색/이분탐색)
 print(' '.join(str(hash_card[m]) if m in hash_card else '0' for m in arr))
