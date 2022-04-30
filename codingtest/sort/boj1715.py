af4ea437bf1f871f304f380dd8ba3fcb4745a3ee
박동규 <102901226+uyggnodkrap@users.noreply.github.com>
2022-04-30 18:30:53 +0900

    유형별 문제풀이 - sort

---
+++ codingtest/sort/boj1715.py
@@ -0,0 +1,28 @@
+"""
+카드 정렬하기
+https://www.acmicpc.net/problem/1715
+"""
+
+import sys
+import heapq
+
+
+cards = []
+N = int(sys.stdin.readline().rstrip())
+for _ in range(N):
+    cards.append(int(sys.stdin.readline().rstrip()))
+
+heapq.heapify(cards)
+
+if N == 1:
+    print(0)
+else:
+    answer = 0
+    while len(cards) > 1:
+        m1 = heapq.heappop(cards)
+        m2 = heapq.heappop(cards)
+        shuffle = m1+m2
+        answer += shuffle
+        heapq.heappush(cards, shuffle)
+
+    print(answer)
